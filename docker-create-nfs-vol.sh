be020f94a138299c5c86497ee77da2d7a172d365
sheaffej <sheaffej@gmail.com>
2021-11-16 16:14:35 -0800

    update docker nfs vol script

--- docker-create-nfs-vol.sh
+++ docker-create-nfs-vol.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Change rw/ro in 2nd --opt as needed
-# Change vol name in last line as needed
+# 1. Change vol name in last line as needed
+# 2. Control read write/only at the NFS server. Setting ro here causes
+# the container to fail to start
+
 docker volume create --driver local \
 --opt type=nfs \
---opt o=addr=192.168.1.120,nfsvers=4,rw \
+--opt o=addr=192.168.1.120,nfsvers=4 \
 --opt device=:/Media \
-somename-nfs-rw
+somename-nfs
